80567324026cc904cc38cb05175a1ede448aa396
Michael Song Mai <michael_song.mai@hsbi.de>
2024-11-21 19:14:09 +0100

    fix kruskal

--- src/algorithms/minimal_spanning_tree/kruskal.py
+++ src/algorithms/minimal_spanning_tree/kruskal.py
@@ -9,7 +9,7 @@ def __reachability_set_table(undirected_graph: UndirectedGraph, edge=None):
         undirected_graph.add_edge(edge)
     reachability_table = {}
     for vertex in undirected_graph.vertices:
-        rset = reachability_set(vertex)
+        rset = reachability_set(undirected_graph, vertex)
         for reachibility_vertex in rset:
             reachability_table[reachibility_vertex.id] = rset
     return reachability_table
@@ -20,8 +20,8 @@ def kruskal(undirected_graph: UndirectedGraph):
     mst = UndirectedGraph(undirected_graph.vertices, set())
     total_weight = 0
     for edge in sorted_edges:
-        if __reachability_set_table(undirected_graph) != \
-             __reachability_set_table(undirected_graph, edge):
+        if __reachability_set_table(mst) != \
+             __reachability_set_table(mst, edge):
             mst.add_edge(edge)
             total_weight += edge.weight
     return mst, total_weight
