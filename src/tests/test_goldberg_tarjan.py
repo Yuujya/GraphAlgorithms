fed826a460e6ff02e7712f1759a7314b6a6a8c9a
Michael Song Mai <michael_song.mai@hsbi.de>
2024-11-25 17:39:51 +0100

    add second test case for maximum flow tests

--- src/tests/test_goldberg_tarjan.py
+++ src/tests/test_goldberg_tarjan.py
@@ -9,10 +9,10 @@ class TestEdmondsKarp(unittest.TestCase):
     def test_compute_maximum_flow(self):
         vertices = {Vertex('s'), Vertex('A'), Vertex('C'),
                     Vertex('B'), Vertex('D'), Vertex('t')}
-        edges = {Edge('s', 'A', 13), Edge('s', 'C', 7), Edge('C', 'A', 6),
-                 Edge('C', 'B', 6), Edge('C', 't', 4), Edge('A', 'B', 7),
-                 Edge('A', 'D', 10), Edge('B', 'D', 8), Edge('B', 't', 3),
-                 Edge('D', 't', 12)}
+        edges = {Edge('s', 'A'), Edge('s', 'C'), Edge('C', 'A'),
+                 Edge('C', 'B'), Edge('C', 't'), Edge('A', 'B'),
+                 Edge('A', 'D'), Edge('B', 'D'), Edge('B', 't'),
+                 Edge('D', 't')}
         capacity = {('s', 'A'): Capacity(0, 13), ('s', 'C'): Capacity(0, 7),
                     ('A', 'B'): Capacity(0, 7), ('C', 'A'): Capacity(0, 6),
                     ('C', 'B'): Capacity(0, 6), ('C', 't'): Capacity(0, 4),
@@ -25,6 +25,30 @@ class TestEdmondsKarp(unittest.TestCase):
                                             start_vertex,
                                             target_vertex)
         expected_flow = 19
+        self.assertEqual(maximum_flow, expected_flow)
+
+    # TODO: muss waehrend einer Iteration der Residualgraph geupdated werden?
+    # also wenn die Flusswerte bereits in der Iteration angepasst wurden
+    def test_compute_maximum_flow2(self):
+        vertices = {Vertex('s'), Vertex('A'), Vertex('C'),
+                    Vertex('B'), Vertex('D'), Vertex('t')}
+        edges = {Edge('s', 'A'), Edge('s', 'B'), Edge('B', 'A'),
+                 Edge('A', 'B'), Edge('C', 'B'), Edge('A', 'C'),
+                 Edge('B', 'D'), Edge('D', 'C'), Edge('C', 't'),
+                 Edge('D', 't')}
+        capacity = {('s', 'A'): Capacity(0, 16), ('s', 'B'): Capacity(0, 13),
+                    ('B', 'A'): Capacity(0, 4), ('A', 'B'): Capacity(0, 10),
+                    ('C', 'B'): Capacity(0, 9), ('A', 'C'): Capacity(0, 12),
+                    ('B', 'D'): Capacity(0, 14), ('D', 'C'): Capacity(0, 7),
+                    ('C', 't'): Capacity(0, 20), ('D', 't'): Capacity(0, 4)}
+        capacitated_network = CapacitatedNetwork(vertices, edges, capacity)
+        start_vertex = Vertex('s')
+        target_vertex = Vertex('t')
+        maximum_flow = compute_maximum_flow(capacitated_network,
+                                            start_vertex,
+                                            target_vertex)
+        expected_flow = 23
+        print(maximum_flow)
         self.assertEqual(maximum_flow, expected_flow)
 
 
